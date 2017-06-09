42400225ae5701d07118f5f95b82730545385f27
Harminder Virk <virk.officials@gmail.com>
2017-06-09 15:22:54 +0530

    feat(request): a request macro to access file or files

--- providers/BodyParserProvider.js
+++ providers/BodyParserProvider.js
@@ -10,15 +10,78 @@
 */
 
 const { ServiceProvider } = require('adonis-bodyparser')
+const _ = require('lodash')
+const FileJar = require('../src/Multipart/FileJar')
 
 class BodyParserProvider extends ServiceProvider {
-  boot () {
-    const Server = this.app.use('Adonis/Src/Server')
-    Server.registerGlobal(['Adonis/Middleware/BodyParser'])
+  /**
+   * Defines the request macro to get an instance of
+   * file.
+   *
+   * @method _defineRequestMacro
+   *
+   * @return {void}
+   *
+   * @private
+   */
+  _defineRequestMacro () {
+    const Request = this.app.use('Adonis/Src/Request')
+
+    /**
+     * Request macro to access a file from the uploaded
+     * files.
+     */
+    Request.macro('file', function (name, options = {}) {
+      const file = _.get(this._files, name)
+      /**
+       * Return null when there is no file
+       */
+      if (!file) {
+        return null
+      }
+
+      /**
+       * Return file when it's a single file
+       */
+      if (!_.isArray(file)) {
+        return file.setOptions(options)
+      }
+
+      /**
+       * Set options to each file when it's an
+       * array of files and instead return
+       * the file jar
+       */
+      file.forEach((eachFile) => {
+        eachFile.setOptions(options)
+      })
+
+      return new FileJar(file)
+    })
   }
 
+  /**
+   * The boot method called by ioc container
+   * as a life-cycle method
+   *
+   * @method boot
+   *
+   * @return {void}
+   */
+  boot () {
+    this._defineRequestMacro()
+  }
+
+  /**
+   * The register method called by ioc container
+   * as a life-cycle method
+   *
+   * @method register
+   *
+   * @return {void}
+   */
   register () {
-    this.app.bind('Adonis/Middleware/BodyParser', () => {
+    this.app.bind('Adonis/Middleware/BodyParser', (app) => {
       const BodyParser = require('../src/BodyParser')
       return new BodyParser(app.use('Adonis/Src/Config'))
     })
