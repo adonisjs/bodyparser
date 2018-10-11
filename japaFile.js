6a061231e978c2570a5a23e7b81d53b1069bae60
Harminder Virk <virk.officials@gmail.com>
2018-10-11 22:41:52 +0200

    chore(package): update dependencies

--- japaFile.js
+++ japaFile.js
@@ -1,13 +1,4 @@
-'use strict'
-
-/*
- * adonis-bodyparser
- *
- * (c) Harminder Virk <virk@adonisjs.com>
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
-*/
-
-const cli = require('japa/cli')
-cli.run('test/**/*.spec.js')
+const { configure } = require('japa')
+configure({
+  files: ['test/**/*.spec.js']
+})
