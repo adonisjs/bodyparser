f2cb88def720d887fb00e5feb882adc4c4d88d1b
Romain Lanz <romain.lanz@pm.me>
2026-02-05 21:03:36 +0100

    style: format files

--- tests/multipart.spec.ts
+++ tests/multipart.spec.ts
@@ -1044,7 +1044,9 @@ test.group('Multipart', () => {
       res.end()
     })
 
-    await supertest(server).post('/').attach('file', fs.basePath + '/fake.png')
+    await supertest(server)
+      .post('/')
+      .attach('file', fs.basePath + '/fake.png')
 
     assert.property(files, 'file')
 
